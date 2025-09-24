a713db7258f96c6a047120d5bc9d0a4c18411b5f
maxcouture02 <113473929+maxcouture02@users.noreply.github.com>
2025-09-24 14:36:30 -0400

    Update +page.svelte

--- src/routes/map/[id]/+page.svelte
+++ src/routes/map/[id]/+page.svelte
@@ -70,8 +70,10 @@
 
 		// Initialize map
 		map = L.map('map').setView([45.4215, -75.6972], 13);
-		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			attribution: '&copy; OpenStreetMap contributors'
+		L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+			subdomains: 'abcd',
+			maxZoom: 20
 		}).addTo(map);
 
 		// Import additional Leaflet components
