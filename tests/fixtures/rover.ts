d10df6511af0a4615cf28fc65f310e74957b7c21
Syed Yead Zaman <s.yead.zaman@gmail.com>
2025-10-03 10:55:48 -0400

    Add rover heartbeat API and corresponding tests

--- tests/fixtures/rover.ts
+++ tests/fixtures/rover.ts
@@ -3,12 +3,14 @@ import { rovers } from '$lib/server/db/schema';
 
 export const createRoverFixture = async (
 	name: string = 'Test Rover',
+	lastHeartbeat: Date = new Date(),
 	ipAddress: string = '127.0.0.1'
 ) => {
 	const result = await db
 		.insert(rovers)
 		.values({
 			name,
+			lastHeartbeat,
 			ipAddress
 		})
 		.returning();
