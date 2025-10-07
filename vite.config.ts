b02670763abb32be848ae8558088f61cbe5a1c7a
Nathan Gawargy <nathan@gawargy.com>
2025-10-07 18:03:04 -0400

    vite server to accept docker internal host requests, update gitignore,

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 
+	server: { // TODO: NATHAN once UI/APIs running in docker container DONT need this anymore
+		host: '0.0.0.0', // allows external connections (e.g., from Docker)
+		port: 5173,
+		allowedHosts: [
+		'localhost',
+		'127.0.0.1',
+		'host.docker.internal' // 👈 allow Docker container requests
+		]
+	},
+
 	test: {
 		environment: 'node',
 		fileParallelism: false,
