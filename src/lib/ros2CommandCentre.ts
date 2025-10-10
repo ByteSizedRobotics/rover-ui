3d35486ea173e2908bcc217618f0ecb4abd2d6c3
Syed Yead Zaman <s.yead.zaman@gmail.com>
2025-10-10 09:52:43 -0400

    Remove 'usb_camera' from required nodes in ROS2CommandCentreClient

--- src/lib/ros2CommandCentre.ts
+++ src/lib/ros2CommandCentre.ts
@@ -722,7 +722,7 @@ export class ROS2CommandCentreClient {
 		console.log('Waiting for required nodes to start up...');
 		
 		// Wait for required nodes to be running (based on Python autonomous_nodes) 
-		const requiredNodes = ['gps', 'csi_camera_1', 'obstacle_detection', 'rover',  'usb_camera']; // TODO: NATHAN add this back'imu' , 'motor_control'
+		const requiredNodes = ['gps', 'csi_camera_1', 'obstacle_detection', 'rover']; // 'usb_camera' TODO: NATHAN add this back'imu' , 'motor_control'
 		const nodesStarted = await this.waitForNodesRunning(requiredNodes, 45000); // 45 second timeout
 		
 		if (!nodesStarted) {
